9599b7501339ca8aa941a8212892ea7f32ccce3e
SoniaTuscano <sonia.tuscano@gmail.com>
2018-02-20 00:49:53 -0500

    first commit

---
+++ history.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Sat Feb 17 18:25:20 2018)---
+runfile('C:/Users/JohnSonia/.spyder-py3/temp.py', wdir='C:/Users/JohnSonia/.spyder-py3')
+
+## ---(Mon Feb 19 23:04:53 2018)---
+runfile('C:/Users/JohnSonia/.spyder-py3/temp.py', wdir='C:/Users/JohnSonia/.spyder-py3')
+20
+runfile('C:/Users/JohnSonia/.spyder-py3/temp.py', wdir='C:/Users/JohnSonia/.spyder-py3')
